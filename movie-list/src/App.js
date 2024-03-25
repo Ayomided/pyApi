3e7cb715aecc79f3f4c6d54ebcc777d98d3a123d
Ayomided <adediji.d@yahoo.com>
2024-03-25 08:17:22 +0000

    form to add to database works

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
+import { MovieForm } from './components/MovieForm'
+import { Container } from 'semantic-ui-react'
 
 function App() {
   const [movies, setMovies] = useState([])
-  
+
   useEffect(() => {
     fetch('/movies').then(response => 
       response.json().then(data => {
@@ -13,13 +15,14 @@ function App() {
     )
   }, [])
 
-  console.log(movies)
-
   return (
     <div className="App">
-      <Movies movies={movies}/>
+      <Container style={{marginTop: 40}}>
+        <MovieForm onNewMovie={movie => setMovies(currentMovies => [...currentMovies, movie])}/>
+        <Movies movies={movies}/>
+      </Container>
     </div>
-  );
+  )
 }
 
 export default App;
